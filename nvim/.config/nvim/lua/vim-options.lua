2df9a9c23dbf2791d27fa96fe41453574d6f241d
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2025-02-10 01:24:40 +0100

    hotkeys for quickfix

--- nvim/.config/nvim/lua/vim-options.lua
+++ nvim/.config/nvim/lua/vim-options.lua
@@ -28,6 +28,9 @@ vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
 vim.keymap.set('n', '<leader>h', ':nohlsearch<CR>')
 vim.wo.number = true
 
+-- Quickfix-list navigation keys
+vim.keymap.set('n', '<M-j>', '<cmd>cnext<CR>zz')
+vim.keymap.set('n', '<M-k>', '<cmd>cprev<CR>zz')
 -- move lines up and down!! Yess
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
