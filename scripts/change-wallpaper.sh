1af24673c3a0b11e937b9514f3079e8049ddcd2c
Niels Fechtel <85522909+nielsfechtel@users.noreply.github.com>
2025-05-18 12:37:32 +0200

    Adjust script

--- scripts/change-wallpaper.sh
+++ scripts/change-wallpaper.sh
@@ -2,27 +2,20 @@
 
 # all this is inspired by My Linux For Work's swww- and pywal-setup
 
+export HOME=/home/niels
+
 # get a random image - do it Quietly, Not changing the wallpaper, In folder X
-~/.local/bin/wal --saturate 0.6 --contrast 11.0 -q -n -i $HOME/wallpaper/
+/home/niels/.local/bin/wal --saturate 0.6 --contrast 11.0 -q -n -i /home/niels/wallpaper/
 
 # get the newly-generated color-schemes in here 
 # below we are only using the wallpaper-variable, which contains the full path
-source "$HOME/.cache/wal/colors.sh"
+source "/home/niels/.cache/wal/colors.sh"
 
 # get wallpaper-image file name, stripping the rest
-new_wallpaper=$(echo $wallpaper | sed "s|$HOME/wallpaper/||g")
+new_wallpaper=$(echo $wallpaper | sed "s|/home/niels/wallpaper/||g")
 
 # this breaks hyprshade, so we turn on auto again (depending on time of day)
 hyprshade auto && swww img $wallpaper --transition-step=20 --transition-fps=60
-hyprpanel useTheme ~/.config/hyprpanel/theme.json
-
-# it seems I can't use @import in wofi's css, so I replace the first 20 lines with the css-lines
-filein=$HOME/.cache/wal/colors-define-color.css
-nline=$(sed -n '$=' $filein)
-for (( i = 1 ; i <= $nline ; i++ )); do
-    line=$(sed -n "${i}p" $filein)
-    sed -i "${i}c\\$line" $HOME/.config/wofi/style.css
-done
-
+hyprpanel useTheme /home/niels/.config/hyprpanel/theme.json
 
 echo "DONE!"
